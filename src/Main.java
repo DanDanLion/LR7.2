c9eb62d03bc434bc57dca5cae6e320c545707f9e
LR1 <nickita.sosnovskiy05@gmail.com>
2023-11-10 19:42:18 +0200

    Усложнонное

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,92 @@
-import java.util.Date;
-
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+import java.util.Arrays;
 public class Main {
-  public static void main(String[] args){
-    BCards met1 = new BCards("Mastercard", "6732456321457321", new Date(2033,1,1), "228","1234");
-    BCards met2 = new BCards("Visa", "3987428988787777", new Date(2033,1,1), "228","1234");
-    BCards met3 = new BCards("НеКарта,но карта", "5167985520230178", new Date(2033,1,1), "228","1234");
-    BCards met4 = new BCards("Artem", "6666666666666666", new Date(2033,1,1), "228","1234");
-    System.out.println(met1.toString());
-    System.out.println("Кількість карток: "+BCards.getCount());
-  }
-}
+    public class BankCard {
+        private String cardType;
+        private String cardNumber;
+        private String expirationDate;
+        private String cvv2;
+        private String pin;
 
+        private static int numberOfCards = 0;
+
+        public BankCard() {
+            numberOfCards++;
+        }
+
+        public BankCard(String cardType, String cardNumber, String expirationDate, String cvv2, String pin) {
+            this.cardType = cardType;
+            this.cardNumber = cardNumber;
+            this.expirationDate = expirationDate;
+            this.cvv2 = cvv2;
+            this.pin = pin;
+            numberOfCards++;
+        }
+
+        public static int getNumberOfCards() {
+            return numberOfCards;
+        }
+        public String toString() {
+            return "Номер картки: " + cardNumber.substring(0, 4) + " " + cardNumber.substring(4, 8) + " " +
+                    cardNumber.substring(8, 12) + " " + cardNumber.substring(12) + "\n" +
+                    "Придатна до: " + expirationDate.substring(0, 2) + "/" + expirationDate.substring(2) + "\n" +
+                    "Тип картки: " + cardType;
+        }
+    }
+
+
+    public class BankCustomer {
+        private String fullName;
+        private String birthdate;
+        private BankCard[] cards;
+
+        public BankCustomer() {
+            this.cards = new BankCard[0];
+        }
+
+        public BankCustomer(String fullName, String birthdate, int numberOfCards) {
+            this.fullName = fullName;
+            this.birthdate = birthdate;
+            this.cards = new BankCard[numberOfCards];
+        }
+
+        public BankCustomer(BankCustomer other) {
+            this.fullName = other.fullName;
+            this.birthdate = other.birthdate;
+            this.cards = Arrays.copyOf(other.cards, other.cards.length);
+        }
+
+        public void addCard(BankCard card) {
+            for (int i = 0; i < cards.length; i++) {
+                if (cards[i] == null) {
+                    cards[i] = card;
+                    break;
+                }
+            }
+        }
+
+        public void makePurchase(String cardNumber, double amount, String pin) {
+            if (cardNumber.length() == 16) {
+                System.out.println("Витрачено " + amount + " грн з картки " + cardNumber);
+            } else {
+                System.out.println("Помилковий номер картки");
+            }
+        }
+
+        public void makeOnlinePurchase(String cardNumber, double amount, String cvv2) {
+            if (cvv2 != null && !cvv2.isEmpty()) {
+                System.out.println("Витрачено " + amount + " грн в інтернет-магазині з картки " + cardNumber);
+            } else {
+                System.out.println("Неправильний CVV2-код");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Main mainInstance = new Main();
+        BankCustomer customer = mainInstance.new BankCustomer();
+        customer.makePurchase("1234567890123456", 50.0, "1111");
+        customer.makeOnlinePurchase("9876543210987654", 30.0, "456");
+    }
+}
